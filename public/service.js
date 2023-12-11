2fca3f125efa665599b698e2441742e0722f1f81
quyetnv <quyetnv@cystack.net>
2023-12-11 09:56:54 +0700

    feat: update service get path

--- public/service.js
+++ public/service.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const path = require('node:path');
 const Store = require('electron-store');
+const isDev = require('electron-is-dev');
 
 const { DesktopService } = require('locker-desktop-service')
 
-class MockStorageService  {
+class MockStorageService {
   storage
   constructor() {
     this.storage = new Store()
@@ -21,12 +22,21 @@ class MockStorageService  {
     return Promise.resolve()
   }
 }
+
+const rootCert = (() => {
+  if (isDev) {
+    return fs.readFileSync(path.join(__dirname, '../cert/ca-cert.pem'))
+  } else {
+    return fs.readFileSync(path.resolve(process.resourcesPath, 'cert', 'ca-cert.pem'))
+  }
+})()
+
 const storageService = new MockStorageService()
 const service = new DesktopService({
   baseApiUrl: `https://api-core.locker.io/v3`,
   storageService,
   ssl: {
-    rootCert: fs.readFileSync(path.join(__dirname, '../cert/ca-cert.pem')),
+    rootCert: rootCert,
   },
   logLevel: 2,
   unsafe: true,
