27bfcb165f005e5ba83834aee0b34b732da9ac4d
quyetnv <quyetnv@cystack.net>
2023-12-19 18:33:04 +0700

    fix: service

--- service/cert-gen-and-service-install.ps1
+++ service/cert-gen-and-service-install.ps1
@@ -2,8 +2,10 @@
 Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -match 'LockerServiceCA' } | Remove-Item
 Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -match 'LockerServiceServer' } | Remove-Item
 
-$ca = New-SelfSignedCertificate -Subject 'CN=LockerServiceCA,O=LockerServiceCA,OU=LockerServiceCA' -CertStoreLocation cert:\LocalMachine\My -KeyExportPolicy Exportable  -KeyUsage CertSign,CRLSign,DigitalSignature -KeyLength 4096 -KeyUsageProperty All -KeyAlgorithm 'RSA'  -HashAlgorithm 'SHA256'  -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider' -FriendlyName desktop-service_ca
-$server = New-SelfSignedCertificate -Subject 'CN=LockerServiceServer,O=LockerServiceServer,OU=LockerServiceServer' -CertStoreLocation cert:\LocalMachine\My -TextExtension @("2.5.29.17={text}IPAddress=0.0.0.0&DNS=*.tls&DNS=localhost") -Signer $ca -KeyUsage KeyEncipherment,DigitalSignature -KeyAlgorithm RSA -KeyLength 4096 -HashAlgorithm "SHA256" -KeyExportPolicy Exportable -FriendlyName desktop-service_server
+$period = (Get-Date).AddYears(100)
+
+$ca = New-SelfSignedCertificate -Subject 'CN=LockerServiceCA,O=LockerServiceCA,OU=LockerServiceCA' -CertStoreLocation cert:\LocalMachine\My -TextExtension @("2.5.29.19={text}CA=1") -KeyExportPolicy Exportable  -KeyUsage CertSign,CRLSign,DigitalSignature -KeyLength 4096 -KeyUsageProperty All -KeyAlgorithm 'RSA'  -HashAlgorithm 'SHA256'  -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider' -FriendlyName locker_service_ca -NotAfter $period
+$server = New-SelfSignedCertificate -Subject 'CN=LockerServiceServer,O=LockerServiceServer,OU=LockerServiceServer' -CertStoreLocation cert:\LocalMachine\My -TextExtension @("2.5.29.17={text}IPAddress=0.0.0.0&DNS=*.tls&DNS=localhost") -Signer $ca -KeyUsage KeyEncipherment,DigitalSignature -KeyAlgorithm RSA -KeyLength 4096 -HashAlgorithm "SHA256" -KeyExportPolicy Exportable -FriendlyName locker_service_server
 $current = Get-Location
 $certDir = "$current\cert"
 if (!(Test-Path $certDir)) {
@@ -42,7 +44,7 @@ $KeyPem | Out-File -Filepath $certDir\server-key.pem -Encoding Ascii
 
 # setup service
 # setup service
-$GatewayPorts = @("14401", "14100", "15601", "14402", "16301", "14504", "14505", "14403")
+$GatewayPorts = @("14411", "14110", "15611", "14412", "16311", "14514", "14515", "14413")
 $WorkingPort = $null
 for ($i=0; $i -lt $GatewayPorts.Length; $i++) {
     # Commands to execute for each item in the array
@@ -55,17 +57,17 @@ for ($i=0; $i -lt $GatewayPorts.Length; $i++) {
     }
     if ($res.StatusCode -eq "200") {
 	$WorkingPort = $GatewayPorts[$i]
-	.\desktop-service.exe -service=stop
-	.\desktop-service.exe -service=uninstall
+	.\locker_service.exe -service=stop
+	.\locker_service.exe -service=uninstall
 	break	    
     }
 }
 
-.\desktop-service.exe -service=install
-.\desktop-service.exe -service=start
+.\locker_service.exe -service=install
+.\locker_service.exe -service=start
 
 if ($WorkingPort -eq $null) {
-    $WorkingPort = "14401"
+    $WorkingPort = "14411"
 }
 
 $uri = "http://localhost:" + $WorkingPort + "/ping-locker-service"
