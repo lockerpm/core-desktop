1188f057fd9164383a9a4e40b54d40864c826428
dangvh <dangvh@cystack.net>
2023-12-20 11:47:15 +0700

    build locker desktop on windows and mac intel

--- deploy.py
+++ deploy.py
@@ -10,8 +10,11 @@ import datetime
 class Builder:
     def __init__(self):
         self.job = os.getenv('CI_JOB_NAME')
+        environment = os.getenv('ENV')
+        self.staging = False if environment == 'release' else True
         self.payload = {
-            "client_id": "cli"
+            "client_id": "desktop",
+            "environment": 'staging' if self.staging else 'prod'
         }
         self.headers = {
             'Authorization': f'Token {os.getenv("VERSION_TOKEN")}',
@@ -22,70 +25,82 @@ class Builder:
         if self.job == 'build_mac_arm64':
             self.os = 'macOS'
             self.architecture = 'arm64'
-            self.local_file = f'locker_mac_arm64-dev'
-            self.public_file = f'locker-cli-mac-arm64-{self.version}'
-            self.commands = [f'GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -ldflags="-s -w -X object.VERSION={self.version}" -o locker_mac_arm64-dev']
+            self.local_file = f'locker-mac-arm64-{self.version}.dmg'
+            self.public_file = f'locker-mac-arm64-{self.version}-{environment}.dmg'
+            if not self.staging:
+                self.commands = ['yarn install', 'yarn build-release-mac-dmg', 'yarn build-release-mac-mas']
+            else:
+                self.commands = ['yarn install', 'yarn build-staging-mac-dmg']
         elif self.job == 'build_mac_x64':
             self.os = 'macOS'
             self.architecture = 'x64'
-            self.local_file = f'locker_mac_x64-dev'
-            self.public_file = f'locker-cli-mac-x64-{self.version}'
-            self.commands = [f'GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w -X object.VERSION={self.version}" -o locker_mac_x64-dev']
+            self.local_file = f'locker-mac-x64-{self.version}.dmg'
+            self.public_file = f'locker-mac-x64-{self.version}-{environment}.dmg'
+            if not self.staging:
+                self.commands = ['mv /Users/locker/locker_service src/services/', 'yarn install', 'yarn release']
+            else:
+                self.commands = ['mv /Users/locker/locker_service src/services/', 'yarn install', 'yarn release']
         elif self.job == 'build_windows_x64':
             self.os = 'Windows'
             self.architecture = 'x64'
-            self.local_file = f'locker_windows-dev.exe'
-            self.public_file = f'locker-cli-win-x64-{self.version}.exe'
-            self.commands = [f'go build -ldflags="-s -w -X object.VERSION={self.version}" -o locker_windows-dev.exe',
-                             f'signtool sign /fd sha256 /a {self.local_file}']
+            self.local_file = f'locker-win-x64-{self.version}.msi'
+            self.public_file = f'locker-win-x64-{self.version}-{environment}.msi'
+            if not self.staging:
+                self.commands = ['mv C:\\dangvh\\locker_service.exe src\\services\\', 'yarn install', 'yarn release']
+            else:
+                self.commands = ['mv C:\\dangvh\\locker_service.exe src\\services\\', 'yarn install', 'yarn release']
         else:
             self.os = 'Linux'
             self.architecture = 'x64'
-            self.local_file = f'locker_linux-dev'
-            self.public_file = f'locker-cli-linux-x64-{self.version}'
-            self.commands = [f'CGO_ENABLED=0 go build -ldflags="-s -w -X object.VERSION={self.version}" -o locker_linux-dev']
+            self.local_file = f'locker-linux-amd64-{self.version}.deb'
+            self.public_file = f'locker-linux-x64-{self.version}-{environment}.deb'
+            if not self.staging:
+                self.commands = ['yarn install', 'yarn build-release-linux-snap',
+                                 f'snapcraft upload --release=beta build/locker-{self.version}.snap',
+                                 'yarn build-release-linux-appimage', 'yarn build-release-linux-deb']
+            else:
+                self.commands = ['mv /home/gitlab-runner/locker_service src/services/', 'yarn install', 'yarn release']
 
     def get_version(self):
-        if self.job == 'build_mac_arm64':
-            self.payload['platform'] = 'mac-arm64'
-        elif self.job == 'build_mac_x64':
-            self.payload['platform'] = 'mac-x64'
-        elif self.job == 'build_windows_x64':
-            self.payload['platform'] = 'windows'
-        else:
-            self.payload['platform'] = 'linux'
-        resp = requests.post(os.getenv("GET_VERSION_URL"), headers=self.headers, json=self.payload).text
+        resp = requests.post(os.getenv('GET_VERSION_URL'), headers=self.headers, json=self.payload).text
         return json.loads(resp)['version']
 
     def update_version(self, success):
         if not success:
             return None
-        self.payload['build'] = success
-        resp = requests.post(os.getenv('UPDATE_VERSION_URL'), headers=self.headers, json=self.payload).text
-        version = json.loads(resp)['version']
-        os.system(f'git tag -a v{version} -m "Tag new version"')
-        os.system(f'git remote set-url origin {os.getenv("ORIGIN")}')
-        os.system(f'git push origin v{version} -o ci.skip')
-        return version
+        if self.get_version() == self.version: # version not updated
+            self.payload['build'] = success
+            resp = requests.post(os.getenv('UPDATE_VERSION_URL'), headers=self.headers, json=self.payload).text
+            return json.loads(resp)['version']
+        else:   # version updated
+            return self.get_version()
 
     def upload(self):
-        if os.path.isfile(self.local_file):
-            config = Config(connect_timeout=3600, read_timeout=3600)
-            client = boto3.client('s3', config=config)
-            client.upload_file(self.local_file, 'lockerio', f'download/{self.public_file}',
-                               ExtraArgs={'ACL': 'public-read'})
+        # try:
+        if os.path.isfile(f'dist/{self.local_file}'):
+            if self.staging:
+                config = Config(connect_timeout=3600, read_timeout=3600)
+                client = boto3.client('s3', config=config)
+                client.upload_file(f'build/{self.local_file}', 'lockerio', f'download/{self.public_file}',
+                                   ExtraArgs={'ACL': 'public-read'})
             return self.update_version(True)
         else:
             return self.update_version(False)
 
     def build(self):
         # if self.os != 'Linux':
+        package_json = json.load(open('package.json'))
+        package_json['version'] = self.version
+        f = open('package.json', 'w')
+        f.write(json.dumps(package_json))
+        f.close()
         for command in self.commands:
             os.system(command)
 
     def notify(self):
-        msg = "New Locker CLI version is available"
+        msg = "New Locker version is available"
         slack_webhook = os.getenv('SLACK_WEBHOOK')
+        github_url = f'https://github.com/lockerpm/desktop-releases/releases/download/v{self.version}/{self.local_file}'
         s3_url = f'https://s.locker.io/download/{self.public_file}'
         payload = {
             "attachments": [
@@ -93,7 +108,7 @@ class Builder:
                     "fallback": msg,
                     "color": "#28a745",
                     "title": msg,
-                    "title_link": s3_url,
+                    "title_link": s3_url if self.staging else github_url,
                     "fields": [
                         {
                             "title": "OS",
@@ -107,7 +122,7 @@ class Builder:
                         },
                         {
                             "title": "Download",
-                            "value": s3_url,
+                            "value": s3_url if self.staging else github_url,
                             "short": True
                         },
                         {
