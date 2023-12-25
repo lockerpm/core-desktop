db823479a503f2af727a511a5f17798e32b21107
dangvh <dangvh@cystack.net>
2023-12-25 14:08:55 +0700

    CI CD for locker desktop including desktop service

--- deploy.py
+++ deploy.py
@@ -62,6 +62,14 @@ class Builder:
                 self.commands = ['cp /home/gitlab-runner/locker_service src/services/', 'yarn install', 'yarn release']
 
     def get_version(self):
+        if self.job == 'build_mac_arm64':
+            self.payload['platform'] = 'mac-arm64'
+        elif self.job == 'build_mac_x64':
+            self.payload['platform'] = 'mac-x64'
+        elif self.job == 'build_windows_x64':
+            self.payload['platform'] = 'windows'
+        else:
+            self.payload['platform'] = 'linux'
         resp = requests.post(os.getenv('GET_VERSION_URL'), headers=self.headers, json=self.payload).text
         return json.loads(resp)['version']
 
